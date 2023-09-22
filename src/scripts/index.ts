e2a3ff0a45241a9ce5c7ca99ad14c032d32aebe1
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-09-22 19:38:15 +0200

    significantly improved styling.

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,7 +5,7 @@ const searchBarElement: HTMLElement | null =
     document.querySelector(".search-bar")
 const searchBarButton: HTMLElement | null =
     document.querySelector(".search-bar-button")
-const cityElement: HTMLElement | null = document.querySelector(".city")
+const cityElement: HTMLHeadingElement | null = document.querySelector(".city")
 const tempElement: HTMLElement | null = document.querySelector(".temp")
 const weatherDescriptionButton: HTMLElement | null = document.querySelector(
     ".weather-description"
@@ -13,17 +13,11 @@ const weatherDescriptionButton: HTMLElement | null = document.querySelector(
 const humidityElement: HTMLElement | null = document.querySelector(".humidity")
 const windElement: HTMLElement | null = document.querySelector(".wind")
 
-// // focus on search bar upon page load.
-// window.addEventListener("load", () => {
-//     searchBarElement?.focus()
-// })
-
 // eventListeners
 searchBarElement?.addEventListener("keydown", (key: KeyboardEvent) => {
     if (key.key === "Enter" && searchBarElement) {
         let inputValue: string = (searchBarElement as HTMLInputElement).value
         search(inputValue)
-        upgradeBackgroundImage(inputValue)
         ;(searchBarElement as HTMLInputElement).value = ""
         console.log(`search for ${inputValue} executed`)
     }
@@ -33,11 +27,13 @@ searchBarButton?.addEventListener("click", () => {
     if (searchBarElement) {
         let inputValue: string = (searchBarElement as HTMLInputElement).value
         search(inputValue)
-        upgradeBackgroundImage(inputValue)
         ;(searchBarElement as HTMLInputElement).value = ""
         console.log(`search for ${inputValue} executed`)
     }
 })
 
 // Create a function that searches the weather API. Need to export the search to pull the right Unsplash image for the background.
-function search(query: string) {}
+function search(query: string) {
+    upgradeBackgroundImage(query)
+    if (cityElement) cityElement.innerText = `Weather in ${query}`
+}
