8d589ab691ba952b929c30336328b7edfb3f2ea5
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-09-24 08:49:25 +0200

    Refactored unsplash_api to work with an internal serverless API. Should work for both now.

---
+++ api/unsplash.js
@@ -0,0 +1,21 @@
+// ./api/unsplash.js
+import fetch from "node-fetch"
+
+module.exports = async (req, res) => {
+    const apiKey = process.env.UNSPLASH_API_KEY
+    const searchQuery = req.query.searchQuery
+
+    try {
+        const apiEndpoint = `https://api.unsplash.com/photos/random?query=${searchQuery}&orientation=landscape&client_id=${apiKey}`
+        const response = await fetch(apiEndpoint)
+
+        if (!response.ok) {
+            throw new Error("Network error")
+        }
+
+        const jsonData = await response.json()
+        res.json(jsonData)
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch Unsplash data" })
+    }
+}
--- src/scripts/unsplash_api.ts
+++ src/scripts/unsplash_api.ts
@@ -18,19 +18,15 @@ export async function upgradeBackgroundImage(
     searchQuery: string
 ): Promise<any> {
     try {
-        let apiKey: string = ""
-        if (process.env.UNSPLASH_API_KEY) {
-            apiKey = process.env.UNSPLASH_API_KEY
-        }
-        console.log(apiKey)
-
-        const apiEndpoint: string = `https://api.unsplash.com/photos/random?query=${searchQuery}&orientation=landscape&client_id=${apiKey}`
+        // Call your serverless function here.
+        const apiEndpoint = `/api/unsplash?searchQuery=${searchQuery}`
 
         const response = await fetch(apiEndpoint)
         if (!response.ok) {
             throw new Error("Network error")
         }
         const jsonData = await response.json()
+
         if (imageElement)
             imageElement.setAttribute("src", jsonData.urls.regular)
         if (imageLink) {
@@ -46,32 +42,38 @@ export async function upgradeBackgroundImage(
         console.error("There was a problem with the Fetch operation:", error)
         return null
     }
-
-    // fetch(apiEndpoint)
-    //     .then((response) => {
-    //         if (!response.ok) {
-    //             throw new Error("Network error")
-    //         }
-    //         console.log(response.json)
-    //         return response.json()
-    //     })
-    //     .then((jsonData) => {
-    //         if (imageElement)
-    //             imageElement.setAttribute("src", jsonData.urls.regular)
-    //         if (imageLink) {
-    //             imageLink?.setAttribute("href", jsonData.links.html)
-    //         }
-
-    //         if (creatorLink) {
-    //             creatorLink?.setAttribute("href", jsonData.user.links.html)
-    //         }
-    //         if (creatorElement)
-    //             creatorElement.innerText = `Photo by ${jsonData.user.name}.`
-    //     })
-    //     .catch((error) => {
-    //         console.error(
-    //             "There was a problem with the Fetch operation:",
-    //             error
-    //         )
-    //     })
 }
+
+// export async function upgradeBackgroundImage(
+//     searchQuery: string
+// ): Promise<any> {
+//     try {
+//         let apiKey: string = ""
+//         if (process.env.UNSPLASH_API_KEY) {
+//             apiKey = process.env.UNSPLASH_API_KEY
+//         }
+//         console.log(apiKey)
+
+//         const apiEndpoint: string = `https://api.unsplash.com/photos/random?query=${searchQuery}&orientation=landscape&client_id=${apiKey}`
+
+//         const response = await fetch(apiEndpoint)
+//         if (!response.ok) {
+//             throw new Error("Network error")
+//         }
+//         const jsonData = await response.json()
+//         if (imageElement)
+//             imageElement.setAttribute("src", jsonData.urls.regular)
+//         if (imageLink) {
+//             imageLink?.setAttribute("href", jsonData.links.html)
+//         }
+//         if (creatorLink) {
+//             creatorLink?.setAttribute("href", jsonData.user.links.html)
+//         }
+//         if (creatorElement)
+//             creatorElement.innerText = `Photo by ${jsonData.user.name}.`
+//         return jsonData
+//     } catch (error) {
+//         console.error("There was a problem with the Fetch operation:", error)
+//         return null
+//     }
+// }
